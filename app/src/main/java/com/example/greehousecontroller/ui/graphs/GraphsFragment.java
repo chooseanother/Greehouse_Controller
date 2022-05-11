8622ac717269acd4ff828101541fd4093edadc6c
david <michalcodavid@gmail.com>
2022-05-11 20:54:13 +0200

    UI for temperature graph

--- app/src/main/java/com/example/greehousecontroller/ui/graphs/GraphsFragment.java
+++ app/src/main/java/com/example/greehousecontroller/ui/graphs/GraphsFragment.java
@@ -12,12 +12,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.greehousecontroller.R;
 import com.example.greehousecontroller.databinding.FragmentGraphsBinding;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class GraphsFragment extends Fragment {
 
     private FragmentGraphsBinding binding;
-
+    LineChart lineChart;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         GraphsViewModel graphsViewModel = new ViewModelProvider(this).get(GraphsViewModel.class);
@@ -25,8 +33,9 @@ public class GraphsFragment extends Fragment {
         binding = FragmentGraphsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGraphs;
-        graphsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        lineChart = root.findViewById(R.id.temperature_lineChart);
+        loadTemperatureChartData(40,60);
+        lineChart.animate();
         return root;
     }
 
@@ -34,5 +43,25 @@ public class GraphsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    private void loadTemperatureChartData(int count,int range ) {
+        ArrayList<Entry> yVals1 = new ArrayList<>();
+        for(int i = 0;i < count;i++){
+            float val = (float) (Math.random()*range)+250;
+            yVals1.add(new Entry(i,val));
+        }
+
+        LineDataSet set1 ;
+
+        set1= new LineDataSet(yVals1,"Temperature");
+        set1.setDrawValues(false);
+        set1.setDrawCircles(false);
+        set1.setDrawHorizontalHighlightIndicator(false);
+        set1.setDrawVerticalHighlightIndicator(false);
+        set1.setColor(0xFFFF0000);
+
+        LineData data = new LineData(set1);
+
+        lineChart.setData(data);
     }
 }
