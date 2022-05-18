e73dc52b972c2e6ac77baa085f534f405b786a19
david <michalcodavid@gmail.com>
2022-05-18 21:23:37 +0200

    Updating GraphsFragment

--- app/src/main/java/com/example/greehousecontroller/ui/view/GraphsFragment.java
+++ app/src/main/java/com/example/greehousecontroller/ui/view/GraphsFragment.java
@@ -10,16 +10,15 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.example.greehousecontroller.R;
-import com.example.greehousecontroller.data.model.Temperature;
 import com.example.greehousecontroller.databinding.FragmentGraphsBinding;
 import com.example.greehousecontroller.ui.viewmodel.GraphsViewModel;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -38,27 +37,40 @@ public class GraphsFragment extends Fragment {
     private FragmentGraphsBinding binding;
     LineChart lineChart;
     GraphsViewModel graphsViewModel;
+    Spinner datesSpinner;
     private final SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM HH:mm", Locale.ENGLISH);
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         graphsViewModel = new ViewModelProvider(this).get(GraphsViewModel.class);
         binding = FragmentGraphsBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
-        Spinner timespanSpinner = view.findViewById(R.id.dates_spinner);
-        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(view.getContext(), R.array.timespans, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        timespanSpinner.setAdapter(adapter);
-
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.timespans,
+                android.R.layout.simple_spinner_item);
         lineChart = view.findViewById(R.id.temperature_lineChart);
+        datesSpinner = view.findViewById(R.id.dates_spinner);
+        datesSpinner.setAdapter(adapter);
         updateMeasurements();
         loadTemperatureChartData(100,100);
         lineChart.animate();
+        datesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos,
+                                       long id) {
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -66,7 +78,10 @@ public class GraphsFragment extends Fragment {
     }
     private void loadTemperatureChartData(int count,int range ) {
         ArrayList<Entry> yVals1 = new ArrayList<>();
-        LineDataSet set1 ;
+        ArrayList<Entry> yVals2 = new ArrayList<>();
+
+        LineDataSet set1;
+        LineDataSet set2;
         XAxis xAxis = lineChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
         xAxis.setTextSize(10f);
@@ -76,35 +91,45 @@ public class GraphsFragment extends Fragment {
         xAxis.setTextColor(Color.rgb(255, 192, 56));
         xAxis.setCenterAxisLabels(true);
         xAxis.setGranularity(1f); // one hour
-        xAxis.setValueFormatter(new MyAxisFormatter());
+        xAxis.setValueFormatter(new Hours());
         for(int i = Objects.requireNonNull(graphsViewModel.getTemperatureHistoryData().getValue()).size()-1; i > 1; i--){
-            System.out.println(graphsViewModel.getTemperatureHistoryData().getValue().get(i).getTime().getTime());
-
-            long now = TimeUnit.MILLISECONDS.toMinutes(graphsViewModel.getTemperatureHistoryData().getValue().get(i).getTime().getTime());
-            yVals1.add(new Entry(now,(float)graphsViewModel.getTemperatureHistoryData().getValue().get(i).getTemperature()));
+            yVals2.add(new Entry(graphsViewModel.getTemperatureHistoryData().getValue().get(i).getTime().getTime(),(float) 0));
+            yVals1.add(new Entry(graphsViewModel.getTemperatureHistoryData().getValue().get(i).getTime().getTime(),(float)graphsViewModel.getTemperatureHistoryData().getValue().get(i).getTemperature()));
         }
         set1 = new LineDataSet(yVals1,"Temperature");
         set1.setDrawValues(false);
-        set1.setDrawCircles(false);
         set1.setDrawHorizontalHighlightIndicator(false);
         set1.setDrawVerticalHighlightIndicator(false);
         set1.setColor(0xFFFF0000);
 
-        LineData data = new LineData(set1);
+
+        set2 = new LineDataSet(yVals2,"");
+        set2.setDrawValues(false);
+        set2.setDrawCircles(false);
+        set2.setDrawHorizontalHighlightIndicator(false);
+        set2.setDrawVerticalHighlightIndicator(false);
+        set2.setColor(0X00);
+
+        LineData data = new LineData(set1,set2);
 
         lineChart.setData(data);
+
+    }
+    private void setValuesOfGraph()
+    {
+
     }
     private void updateMeasurements(){
         // TODO: Figure out how to handle greenhouseId
         graphsViewModel.updateTemperatureHistoryData("test");
     }
-    private static class MyAxisFormatter extends ValueFormatter {
-        private final SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM HH:mm:ss", Locale.ENGLISH);
+    private static class Hours extends ValueFormatter {
+        private final SimpleDateFormat mFormat = new SimpleDateFormat("dd.MM HH:mm:ss", Locale.ENGLISH);
 
         @Override
         public String getFormattedValue(float value) {
-            long millis = TimeUnit.HOURS.toMillis((long) value);
-            return mFormat.format(new Date(millis));
+            return mFormat.format(new Date((long) value));
         }
     }
+
 }
