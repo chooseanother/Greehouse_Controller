5d06703d963d6045acdf7c8298ed929bd318c240
Kim Tranberg <kim.tranberg@gmail.com>
2022-05-27 12:37:17 +0200

    Basic JUnit test for temperature

--- app/src/test/java/com/example/greehousecontroller/ExampleUnitTest.java
+++ app/src/test/java/com/example/greehousecontroller/ExampleUnitTest.java
@@ -1,17 +1,35 @@
 package com.example.greehousecontroller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import android.content.Context;
+
+import com.example.greehousecontroller.data.model.UserInfo;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+    @Mock
+    Context mockContext;
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void readStringFromContext_LocalizedString(){
+        when(mockContext.getString(R.string.greenhouse_id)).thenReturn("test");
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setGreenhouseID(mockContext.getString(R.string.greenhouse_id));
+        String result = userInfo.getGreenhouseID();
+        assertEquals("test",result);
     }
 }
