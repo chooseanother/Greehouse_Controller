a378d259f0d7665b95ffc7e8904006ba828eb9e8
Maggie <maki.lapcik@gmail.com>
2022-05-27 15:03:40 +0200

    add pot fragment cleaned up

--- app/src/main/java/com/example/greehousecontroller/ui/view/AddPotFragment.java
+++ app/src/main/java/com/example/greehousecontroller/ui/view/AddPotFragment.java
@@ -26,14 +26,17 @@ public class AddPotFragment extends Fragment {
 
     private AddPotViewModel viewModel;
     private FragmentAddPotBinding binding;
+    private NavController navController;
+    private String greenhouseId;
+
+    //xml elements
     private EditText namePotTextView;
     private EditText minimalHumidityTextView;
     private Button savePotButton;
     private Button cancelPotButton;
-    private NavController navController;
     private View root;
     private Spinner spinner;
-    private String greenhouseid;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -41,41 +44,47 @@ public class AddPotFragment extends Fragment {
                 new ViewModelProvider(this).get(AddPotViewModel.class);
         binding = FragmentAddPotBinding.inflate(inflater, container, false);
         root = binding.getRoot();
-        getGreenhouseID();
-        namePotTextView = root.findViewById(R.id.potNameTextView);
-        minimalHumidityTextView = root.findViewById(R.id.minimalHumidityTextView);
-        minimalHumidityTextView.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-        savePotButton = root.findViewById(R.id.saveAddPotButton);
-        cancelPotButton = root.findViewById(R.id.cancelPotButton);
-        spinner = root.findViewById(R.id.sensorSpinner);
-        ArrayList<String> sensors = (ArrayList<String>) viewModel.getAvailableSensors();
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), com.anychart.R.layout.support_simple_spinner_dropdown_item, sensors);
-        spinner.setAdapter(dataAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
         navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
 
+        getGreenhouseID();
+        setUpBinding();
+        setUpSpinner();
+        setUpListeners();
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+    private void getGreenhouseID(){
+        viewModel.initUserInfo();
+        viewModel.getUserInfo().observe(getViewLifecycleOwner(), userInfo -> {
+            greenhouseId = userInfo.getGreenhouseID();
+        });
+    }
+
+    private void setUpBinding(){
+        namePotTextView = binding.potNameTextView;
+        minimalHumidityTextView = binding.minimalHumidityTextView;
+        minimalHumidityTextView.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        savePotButton = binding.saveAddPotButton;
+        cancelPotButton = binding.cancelPotButton;
+        spinner = binding.sensorSpinner;
+    }
+
+    private void setUpListeners(){
         savePotButton.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 boolean result;
                 if(spinner.getSelectedItem() == null){
-                     result = viewModel.validInput(greenhouseid, null , namePotTextView.getText().toString(),
+                    result = viewModel.validInput(greenhouseId, null , namePotTextView.getText().toString(),
                             minimalHumidityTextView.getText().toString());
                 }
                 else
                 {
-                     result = viewModel.validInput(greenhouseid, spinner.getSelectedItem().toString() , namePotTextView.getText().toString(),
+                    result = viewModel.validInput(greenhouseId, spinner.getSelectedItem().toString() , namePotTextView.getText().toString(),
                             minimalHumidityTextView.getText().toString());
                 }
                 if(result){
@@ -90,17 +99,23 @@ public class AddPotFragment extends Fragment {
                 navController.navigate(R.id.nav_home);
             }
         });
-        return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-    private void getGreenhouseID(){
-        viewModel.initUserInfo();
-        viewModel.getUserInfo().observe(getViewLifecycleOwner(), userInfo -> {
-            greenhouseid = userInfo.getGreenhouseID();
+    private void setUpSpinner(){
+        ArrayList<String> sensors = (ArrayList<String>) viewModel.getAvailableSensors();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), com.anychart.R.layout.support_simple_spinner_dropdown_item, sensors);
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
     }
 }
