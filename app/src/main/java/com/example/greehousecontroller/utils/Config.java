297822f7ee3a7edd1bbe77a00e7a627d07dbfef5
Kim Tranberg <kim.tranberg@gmail.com>
2022-05-30 09:04:15 +0200

    Limited maximum pots to 6

--- app/src/main/java/com/example/greehousecontroller/utils/Config.java
+++ app/src/main/java/com/example/greehousecontroller/utils/Config.java
@@ -3,5 +3,5 @@ package com.example.greehousecontroller.utils;
 public class Config {
     public static final String FIREBASE_DB_URL = "https://greehouse-controller-default-rtdb.europe-west1.firebasedatabase.app/";
     public static final String API_BASE_URL = "http://sepdemo2-env.eba-n9rqip8w.eu-central-1.elasticbeanstalk.com";
-    public static final int MAX_NUMBER_OF_POTS = 60;
+    public static final int MAX_NUMBER_OF_POTS = 6;
 }
